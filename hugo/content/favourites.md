864725a8d56f78615073d3d539dd314e31a503ad
Sean Dockray <sean@e-rat.org>
2020-11-18 16:43:29 +1100

    favoriting functionality

--- hugo/content/favourites.md
+++ hugo/content/favourites.md
@@ -6,3 +6,5 @@ title: "Favourites"
 
 
 <h2>Click on <span class="plus"></span> next to graduate name to save your favourites here.</h2>
+
+<div id="favorites-listing"></div>
--- hugo/themes/amplified-together/assets/js/main.js
+++ hugo/themes/amplified-together/assets/js/main.js
@@ -19,5 +19,67 @@
       $('#nav-toggle').on('click', function() {
         this.classList.toggle('active');
       });
+
+      // Builds the fave data for an element
+      function buildFave($ele) {
+        return {
+          studentname: $ele.attr('data-studentname').trim(),
+          img: $ele.attr('data-img').trim(),
+          url: $ele.attr('data-url').trim()
+        }
+      }
+
+      // Does the actual check against localstorage
+      function isFaved(f) {
+        var faves = (localStorage.getItem('faves')) 
+          ? JSON.parse(localStorage.getItem('faves'))
+          : []
+        for (fave of faves) {
+          if (fave.studentname == f.studentname) {
+            return true
+          }
+        }
+        return false
+      }
+
+      // Add class to already faved
+      $('.add-favorite').each(function() {
+        var fave = buildFave($(this))
+        if (isFaved(fave)) {
+          $(this).addClass('faved')
+        }
+      })
+
+      // Add to favorites
+      $('.add-favorite').click(function(e) {
+        var faves = (localStorage.getItem('faves')) 
+          ? JSON.parse(localStorage.getItem('faves'))
+          : []
+        var fave = buildFave($(this))
+        if (!isFaved(fave)) {
+          faves.push(fave)
+          localStorage.setItem('faves', JSON.stringify(faves))
+          console.log(fave.studentname, "ADDED to faves")
+        } else {
+          console.log(fave.studentname, "is already a fave")
+        }
+      })
+
+      // Show favorites!
+      $('#favorites-listing').each(function() {
+        var faves = (localStorage.getItem('faves')) 
+          ? JSON.parse(localStorage.getItem('faves'))
+          : []
+        for (fave of faves) {
+          $student = $('<div class="student preview"></div>')
+          $imgdiv = $('<div class="image"></div>')
+          $img = $('<img src="' + fave.img + '">')
+          $name = $('<a href="' + fave.url + '">' + fave.studentname + '</a>')
+          $img.appendTo($imgdiv)
+          $imgdiv.appendTo($student)
+          $name.appendTo($student)
+          $student.appendTo($(this))
+        }
+      })
     }); // end DOM ready
   })(jQuery); // end jQuery
